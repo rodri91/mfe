66d25424af88684b6c71ab1d7a455505a1d82506
Rodrigo Costanzo <yodri@Rodrigos-MBP-2.fibertel.com.ar>
2022-06-09 15:33:56 -0300

    add auth subapp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,8 +1,8 @@
-import React, {useRef, useEffect} from 'react';
-import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+import { mount } from 'auth/AuthApp';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({onSignIn}) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -15,12 +15,12 @@ export default () => {
         if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
-      }
+      },
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
-  }, []);
+  })
 
-  return <div ref={ref} />
+  return <div ref={ref}/>
 }
-
